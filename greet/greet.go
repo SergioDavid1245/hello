123d7d4130650edb2ee43908910edfb3e7812596
SergioDavid1245 <a20170242@pucp.edu.pe>
2021-08-09 23:08:33 -0500

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "-"
+
+// English retorna saludo en inglés
+func English() string {
+	return "Hi" + emoji
+}
+
+// Italian retorna saludo en italiano
+func Italian() string {
+	return "Ciao" + emoji
+}
